f502a80bfea5d8672270c53cf8e391b483819fb1
Kamiya Keisuke <luckey.star123@gmail.com>
2016-10-13 00:25:43 +0900

    三角波

---
+++ triangle_wave.c
@@ -0,0 +1,33 @@
+//
+// Created by Kamiya Keisuke on 2016/10/13.
+//
+
+#include "header/fileIO.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define F0 500
+
+int main(){
+  PCM pcm;
+  int i, n;
+
+  pcm.fs = 44100;
+  pcm.bit = 16;
+  pcm.len = pcm.fs * 1;
+  pcm.s = (double *)calloc(pcm.len, sizeof(double));
+
+  for(i = 1; i <= 44 ; i+=2){
+    for (int n = 0; n < pcm.len ; ++n) {
+      pcm.s[n] += 1.0 / i / i * sin(i * M_PI / 2 ) * sin(2.0 * M_PI * i * F0 * n / pcm.fs);
+    }
+  }
+
+  for(i = 0 ; i < pcm.len ; i++) pcm.s[i] *= 0.1;
+
+  write_wave_mono(&pcm, "triangle_curve.wav");
+
+  free(pcm.s);
+  return 0;
+}
