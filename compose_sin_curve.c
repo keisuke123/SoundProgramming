dfbff7768b239f9de0ac62e242959de02662ff97
Kamiya Keisuke <luckey.star123@gmail.com>
2016-10-13 14:59:47 +0900

    サイン波の合成(500Hz, 1000Hz)の合成例

--- compose_sin_curve.c
+++ compose_sin_curve.c
@@ -10,15 +10,27 @@
 int main(){
   PCM pcm;
 
+  // 量子化ビット数
   pcm.bit = 16;
+
+  // サンプリング周波数
   pcm.fs = 44100;
+
+  // 長さ
   pcm.len = pcm.fs * 1;
+
+  // 音声データ領域をcallocで確保
   pcm.s = (double *)calloc(pcm.len, sizeof(double));
 
+  // 500Hzと1000Hzのサイン波を合成
   sin_curve(&pcm, 0.1, 500.0);
+  sin_curve(&pcm, 0.1, 1000.0);
 
+  // 書き出し
   write_wave_mono(&pcm, "sin.wav");
 
+  // 領域の開放
   free(pcm.s);
+
   return 0;
 }
