2f204a763a563a1b87d660c16b803f06bc4801db
Kamiya Keisuke <luckey.star123@gmail.com>
2016-10-13 14:44:12 +0900

    波形追加

--- header/waves.c
+++ header/waves.c
@@ -11,8 +11,38 @@ void sin_curve(PCM *pcm, double gain, double f0){
   int n;
 
   for (n = 0; n < pcm->len; ++n) {
-    pcm->s[n] = gain * sin(2 * M_PI * f0 * n / pcm->fs);
+    pcm->s[n] += gain * sin(2 * M_PI * f0 * n / pcm->fs);
   }
 
   return;
+}
+
+void square_curve(PCM *pcm, double gain, double f0) {
+  int i, n;
+  
+  for(i = 1; i <= 44 ; i+=2){
+    for (int n = 0; n < pcm->len ; ++n) {
+      pcm->s[n] += 1.0 / i * sin(2.0 * M_PI * i * F0 * n / pcm->fs);
+    }
+  }
+}
+
+void triangle_curve(PCM *pcm, double gain, double f0) {
+  int i, n;
+
+  for(i = 1; i <= 44 ; i+=2){
+    for (int n = 0; n < pcm->len ; ++n) {
+      pcm->s[n] += 1.0 / i / i * sin(i * M_PI / 2 ) * sin(2.0 * M_PI * i * F0 * n / pcm->fs);
+    }
+  }
+}
+
+void sawtooth_curve(PCM *pcm, double gain, double f0) {
+  int i, n;
+  
+  for (i = 1; i <= 44; ++i) {
+    for(n = 0 ; n < pcm->len ; n++){
+      pcm->s[n] += 1.0 / i * sin(2.0 * M_PI * i * F0 * n / pcm->fs);
+    }
+  }
 }
